7c6adcd766e382fab30959b9ff32dfeefa3c72ad
Yabiten <iabiten@gmail.com>
2015-12-16 21:19:55 +0000

    add tests

---
+++ Composite/Calc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    public class Calc
+    {
+        public static double Moy(int[] tab)
+        {
+            int sum = 0;
+            for (int i = 0; i < tab.Length; i++)
+            {
+                sum += tab[i];
+            }
+            return sum /(double) tab.Length;
+        }
+
+    }
+}
---
+++ CompositeTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Composite;
+
+namespace CompositeTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestAverage()
+        {
+            int[] t = new[] { 1, 12, 31, 6 };
+            Assert.AreEqual(12.5, Calc.Moy(t));
+        }
+    }
+}
